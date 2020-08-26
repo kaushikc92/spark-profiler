03fa23be174667d9a8ebf1c83436c6b5102b4758
EC2 Default User <ec2-user@ip-172-31-43-219.ec2.internal>
2020-08-26 12:10:04 +0000

    Update

--- profiler.py
+++ profiler.py
@@ -1,3 +1,7 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("profiler").getOrCreate()
 sc = spark.sparkContext
+with open('/opt/spark/data/lake/courses/reed.csv', 'r') as f:
+    data = f.read()
+df = spark.read.csv(sc.parallelize(data.splitlines()))
+df.show()
