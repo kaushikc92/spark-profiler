13b76ca121768ad7d8febe9ed4826a9c99734dca
EC2 Default User <ec2-user@ip-172-31-43-219.ec2.internal>
2020-08-27 02:19:20 +0000

    Update

--- profiler.py
+++ profiler.py
@@ -1,7 +1,48 @@
+import glob, subprocess, random, csv
+
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("profiler").getOrCreate()
 sc = spark.sparkContext
-with open('/opt/spark/data/lake/courses/reed.csv', 'r') as f:
-    data = f.read()
-df = spark.read.csv(sc.parallelize(data.splitlines()))
-df.show()
+
+ROOT_LAKE_DIR = "/opt/spark/data/lake"
+LAKE = "courses"
+LAKE_DIR = "{}/{}".format(ROOT_LAKE_DIR, LAKE)
+
+def get_sample(root_lake_dir, table_name, sample_size):
+    file_path = "{}/{}".format(root_lake_dir, table_name)
+    stdoutdata = subprocess.check_output(['wc', '-l', file_name])
+    n_lines = stdoutdata.split()[0]
+    try:
+        f = open(file_path, encoding="utf-8")
+    except UnicodeDecodeError:
+        f = open(file_path, encoding="ISO-8859-1")
+    result = list()
+    reader = csv.reader(f)
+    result.append(table_name)
+    result.append(next(reader))
+    if sample_size > n_lines:
+        for line in reader:
+            result.append(line)
+    else:
+        p = float(sample_size) / float(num_lines)
+        for line in reader:
+            if random.random() < p:
+                result.append(line)
+    f.close()
+
+def map_table(rows_list):
+    final = [map_column(i, rows_list[0], rows_list[2:], column) for i,column in enumerate(rows_list[1])]
+    return final
+
+def map_column(i, table_name, rows_list, column):
+    trait = list()
+    trait.append(table_name)
+    trait.append(column)
+    return trait
+
+def search_dir(dir_path):
+    files = glob.glob("{}/*.csv".format(dir_path), recursive=True)
+    return list(map(lambda x: os.path.relpath(x, dir_path), files))
+
+tables_rdd = sc.parallelize(search_dir(LAKE_DIR))
+profiles_rdd = tables_rdd.flatMap(lambda x: map_table(get_sample(ROOT_LAKE_DIR, x, 1000)))
